829c542d36a4efd0c43aa397f1da80a179c10304
FenilParakhiya02 <pfenil@my.centennialcollege.ca>
2022-10-30 11:14:28 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+/*
+db.js
+Fenil Parakhiya
+301240778
+Favourite Book List web app
+*/
+
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://fenilparakhiya:p2MpFhnhbhxbvIUA@cluster0.o4ep0ys.mongodb.net/?retryWrites=true&w=majority"
+ 
+};
